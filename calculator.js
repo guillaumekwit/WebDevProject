084264000ea953122382a53d2288618f584e1c8d
Robert <mulderr@wit.edu>
2022-04-08 13:22:53 -0400

    fixed submit issue
    changed submit element to a button, calculator will no longer break on browsers other than Firefox

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-	const calcForm = document.getElementById("calcForm");
-	calcForm.addEventListener("submit", function(e){
+	const calcButton = document.getElementById("submit");
+	calcButton.addEventListener("click", function(e){
 		let firstWeight = document.getElementById("weight").value;
 		let firstReps = document.getElementById("reps").value;
 
@@ -8,11 +8,11 @@ window.onload = function(){
 		let oneRepMax = calculateOneRep(firstWeight, firstReps);
 
 		//change h2 to list one rep max weight
-		var ormHeader = document.getElementById("ormHeader");
+		let ormHeader = document.getElementById("ormHeader");
 		ormHeader.innerHTML = "Your one-rep max is: " + oneRepMax;
 
 		//if table already exists, delete
-		var existingRows = document.querySelectorAll('tr');
+		let existingRows = document.querySelectorAll('tr');
 		for (let i=0; i<existingRows.length; i++){
 			let columns = existingRows[i].querySelectorAll('td');
 			for (let j=0; j<columns.length; j++){
@@ -32,8 +32,8 @@ window.onload = function(){
 		let unit = document.getElementById("unit").value; //grabs unit (kg or lb)
 
 		for (i=1; i<=30; i++){
-			var weight = calculateWeight(oneRepMax, i)
-			var percent = calculatePercent(i);
+			let weightText = calculateWeight(oneRepMax, i)
+			let percentText = calculatePercent(i);
 			
 			//adds row to table
 			let row = table.insertRow(i-1);
@@ -42,27 +42,27 @@ window.onload = function(){
 			let col3 = row.insertCell(2);
 
 			col1.innerHTML = i;
-			col2.innerHTML = weight + " " + unit;
-			col3.innerHTML = percent + "%";
+			col2.innerHTML = weightText + " " + unit;
+			col3.innerHTML = percentText + "%";
 		}
 
 	});
 }
 
 function calculateOneRep(weight, reps){ //calculates one rep max
-	var percent = 100 - (reps * 2.5);
-	var max = (weight / (percent/100)).toFixed(2).replace(/[.,]00/,"");
+	let percent = 100 - (reps * 2.5);
+	let max = (weight / (percent/100)).toFixed(2).replace(/[.,]00/,"");
 	return max;
 }
 
 function calculateWeight(maxWeight, reps){
-	var percent = 100 - (reps * 2.5);
-	var weight = (maxWeight * (percent/100)).toFixed(2).replace(/[.,]00/,"");
+	let percent = 100 - (reps * 2.5);
+	let weight = (maxWeight * (percent/100)).toFixed(2).replace(/[.,]00/,"");
 	return weight;
 }
 
 function calculatePercent(reps){
-	var percent = (100 - (reps * 2.5)).toFixed(0);
+	let percent = (100 - (reps * 2.5)).toFixed(0);
 	return percent;
 }
 	
