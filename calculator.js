182250ec5517b481d941f066d7da2d995984d09d
Robert <mulderr@wit.edu>
2022-04-10 21:28:02 -0400

    various fixes
    -fixed calculator sizing issue
    -added some styling to calculator table
    -added sidebar to calculator
    -fixed spelling on sidebar (1RM Calcualtor -> 1RM Calculator) on both calculator page and main page
    -tried to fix overuse of divs on 1RM.html but there was too much that caused random styling issues

--- calculator.js
+++ calculator.js
@@ -4,46 +4,51 @@ window.onload = function(){
 		let firstWeight = document.getElementById("weight").value;
 		let firstReps = document.getElementById("reps").value;
 
-		//get one rep max
-		let oneRepMax = calculateOneRep(firstWeight, firstReps);
+		if ((firstReps >= 1 && firstReps <=30) && (firstWeight >= 1 && firstWeight <= 1500)){
 
-		//change h2 to list one rep max weight
-		let unit = document.getElementById("unit").value; //grabs unit (kg or lb)
-		let ormHeader = document.getElementById("ormHeader");
-		ormHeader.innerHTML = "Your one-rep max is: " + oneRepMax + " " + unit + "s";
+			//get one rep max
+			let oneRepMax = calculateOneRep(firstWeight, firstReps);
 
-		//if table already exists, delete
-		let existingRows = document.querySelectorAll('tr');
-		for (let i=0; i<existingRows.length; i++){
-			let columns = existingRows[i].querySelectorAll('td');
-			for (let j=0; j<columns.length; j++){
-				existingRows[i].removeChild(columns[j]);
+			//change h2 to list one rep max weight
+			let unit = document.getElementById("unit").value; //grabs unit (kg or lb)
+			let ormHeader = document.getElementById("ormHeader");
+			ormHeader.innerHTML = "Your one-rep max is: " + oneRepMax + " " + unit + "s";
+
+			//if table already exists, delete
+			let existingRows = document.querySelectorAll('tr');
+			for (let i=0; i<existingRows.length; i++){
+				let columns = existingRows[i].querySelectorAll('td');
+				for (let j=0; j<columns.length; j++){
+					existingRows[i].removeChild(columns[j]);
+				}
 			}
-		}
 
-		//fill in table header
-		let tableHeader = document.getElementsByTagName("th");
-		tableHeader[0].innerHTML = "Repetitions";
-		tableHeader[1].innerHTML = "Weight";
-		tableHeader[2].innerHTML = "Percentage of 1RM";
+			//fill in table header
+			let tableHeader = document.getElementsByTagName("th");
+			tableHeader[0].innerHTML = "Repetitions";
+			tableHeader[1].innerHTML = "Weight";
+			tableHeader[2].innerHTML = "Percentage of 1RM";
 
 
-		//initialize table
-		let table = document.getElementById("tableBody");
-		
-		for (i=1; i<=30; i++){
-			let weightText = calculateWeight(oneRepMax, i)
-			let percentText = calculatePercent(i);
+			//initialize table
+			let table = document.getElementById("tableBody");
 			
-			//adds row to table
-			let row = table.insertRow(i-1);
-			let col1 = row.insertCell(0);
-			let col2 = row.insertCell(1);
-			let col3 = row.insertCell(2);
+			for (i=1; i<=30; i++){
+				let weightText = calculateWeight(oneRepMax, i)
+				let percentText = calculatePercent(i);
+				
+				//adds row to table
+				let row = table.insertRow(i-1);
+				let col1 = row.insertCell(0);
+				let col2 = row.insertCell(1);
+				let col3 = row.insertCell(2);
 
-			col1.innerHTML = i;
-			col2.innerHTML = weightText + " " + unit;
-			col3.innerHTML = percentText + "%";
+				col1.innerHTML = i;
+				col2.innerHTML = weightText + " " + unit;
+				col3.innerHTML = percentText + "%";
+			}
+		} else {
+			alert("Lift must be 1 to 1500 \r\nReps must be 1 to 30");
 		}
 
 	});
